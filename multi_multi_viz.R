743b9791312d6c50216334cf1d0da1405d5b24ae
Chris McGinnis <cmcginni92@gmail.com>
2026-05-08 15:46:28 -0700

    Add files via upload

--- multi_multi_viz.R
+++ multi_multi_viz.R
@@ -1,6 +1,6 @@
 ################################################################################
 ## Sample multiplexing for joint RNA-ATAC multiomic profiling using MULTI-seq ##
-## Nature Protocols (2026) ###### Chris McGinnis, PHD ###### May 2026 ##########
+## Nature Protocols (2026) ###### Chris McGinnis, PhD ###### May 2026 ##########
 ################################################################################
 
 library(Seurat)
@@ -8,25 +8,6 @@ library(Signac)
 library(deMULTIplex)
 library(deMULTIplex2)
 library(ggplot2)
-
-
-# Tag 1: CGAAGCCC - k562-A
-# Tag 2: ACATGCGT - k562-B
-# Tag 3: TAAGGCTC - thp1
-# Tag 4: CCATGGCG - jurkat-A
-# Tag 5: AAAGGGGA - jurkat-B
-# Tag 6: TTACGGTG - jurkat_stim-A
-# Tag 7: GCATGTAC - jurkat_stim-B
-# Tag 8: CAAGGTCT - telohaec_crispri
-
-
-
-
-
-
-
-
-
 
 ##############
 ## Figure 4 ##
@@ -84,35 +65,17 @@ ggplot(neg_enrich, aes(x=multi, y=freq)) + geom_jitter(position = jitter_pos) + 
 
 ## Fig. 4I: Post-QC joint RNA-ATAC UMAP colored by MULTI-seq classification with marker gene violin plots
 # load('seu_postQC.Robj)
-DimPlot(seu_postQC, group.by = 'multi', cols=c('grey','black','goldenrod','palegreen3','seagreen','dodgerblue','cadetblue3','orchid4','darkred','lightcoral'))+NoLegend()+NoAxes()+theme(plot.title = element_blank())
+# seu_postQC <- SetIdent(seu_postQC, value=seu_sub1$multi)
+DimPlot(seu_postQC, cols=c('grey','black','goldenrod','palegreen3','seagreen','dodgerblue','cadetblue3','orchid4','darkred','lightcoral'))+NoLegend()+NoAxes()+theme(plot.title = element_blank())
 VlnPlot(seu_postQC, pt.size = 0, idents=paste0('Tag',1:8), features=rev(c('CPED1','FOXP2','PSAT1','DPF3','FBXL7')), stack=T, fill.by='ident', cols=c('goldenrod','palegreen3','seagreen','dodgerblue','cadetblue3','orchid4','darkred','lightcoral')) + NoLegend() 
 
 ## Fig. 4J: Subset joint RNA-ATAC UMAP colored by MULTI-seq classification
 # load('seu_sub1.Robj')
-DimPlot(seu_sub1, group.by = 'multi', cells=colnames(seu_sub1)[which(seu_sub1$multi %in% c('multiplet','negative',paste0('Tag',4:7)))], cols=c('grey','cadetblue3','dodgerblue','seagreen','palegreen3','black'), order='multiplet')+NoLegend()+NoAxes()+theme(plot.title = element_blank())
+# seu_sub1 <- SetIdent(seu_sub1, value=seu_sub1$multi)
+DimPlot(seu_sub1, cells=colnames(seu_sub1)[which(seu_sub1$multi %in% c('multiplet','negative',paste0('Tag',4:7)))], cols=c('grey','cadetblue3','dodgerblue','seagreen','palegreen3','black'), order='multiplet')+NoLegend()+NoAxes()+theme(plot.title = element_blank())
+# seu_sub1 <- SetIdent(seu_sub1, value=seu_sub1$predicted_dub)
 VlnPlot(seu_sub1, pt.size = 0, idents=c('dub',paste0('Tag',4:7)),features=rev(c('CPED1','FOXP2','PSAT1','DPF3','FBXL7')), stack=T, fill.by='ident', cols=rev(c('black','cadetblue3','dodgerblue','seagreen','palegreen3'))) + NoLegend() 
 
 ## Fig. 4K: Final cleaned UMAP
 # load('seu_final.Robj')
 DimPlot(seu_final, group.by = 'multi', cols=c('goldenrod','palegreen3','seagreen','dodgerblue','cadetblue3','orchid4','darkred','lightcoral'))+NoLegend()+NoAxes()+theme(plot.title = element_blank())
-
-DimPlot(seu_sub1, group.by = 'multi', cells=colnames(seu_sub1)[which(seu_sub1$multi %in% c('multiplet','negative',paste0('Tag',4:7)))], cols=c('grey','cadetblue3','dodgerblue','seagreen','palegreen3','black'), order='multiplet')+NoLegend()+NoAxes()+theme(plot.title = element_blank())
-
-DefaultAssay(seu_final) <- "RNA"
-seu_final <- NormalizeData(seu_final)
-seu_final <- ScaleData(seu_final); gc()
-seu_final <- FindVariableFeatures(seu_final)
-seu_final <- RunPCA(seu_final)
-DefaultAssay(seu_final) <- "ATAC"
-seu_final <- FindTopFeatures(seu_final, min.cutoff = 5)
-seu_final <- RunTFIDF(seu_final)
-seu_final <- RunSVD(seu_final)
-seu_final <- FindMultiModalNeighbors(seu_final, reduction.list = list("pca", "lsi"), dims.list = list(1:30, 2:40), modality.weight.name = "RNA.weight", verbose = TRUE)
-seu_final <- RunUMAP(seu_final, nn.name = "weighted.nn", assay = "RNA", verbose = TRUE)
-seu_final <- FindClusters(seu_final, graph.name = "wsnn", algorithm = 3, resolution = 0.5, verbose = FALSE)
-save(seu_final, file='seu_final.Robj')
-
-
-
-
-
